e063a6980abb0c5600baba946fd654f99c4831de
Michael <Michael@DESKTOP-9V9NRVR.home>
2017-03-28 16:23:55 +0200

    Smiley hinzugefügt

--- src/michael/Test1703.java
+++ src/michael/Test1703.java
@@ -8,7 +8,7 @@ public class Test1703 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hallo Michael");
+		System.out.println("Hallo Michael :)");
 	}
 
 }

1e04aad150daed0667707ced02b3436433f2f69b
Michael <Michael@DESKTOP-9V9NRVR.home>
2017-03-28 16:18:08 +0200

    Version 1

---
+++ src/michael/Test1703.java
@@ -0,0 +1,14 @@
+package michael;
+
+public class Test1703 {
+
+	public Test1703() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hallo Michael");
+	}
+
+}

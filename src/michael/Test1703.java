2a9249a803469f16cd18a60a55d8f2503aa39df9
Michael <Michael@DESKTOP-9V9NRVR.home>
2017-03-28 16:21:08 +0200

    2. Version
    3mal S.y.p.

--- src/michael/Test1703.java
+++ src/michael/Test1703.java
@@ -9,6 +9,8 @@ public class Test1703 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hallo Michael");
+		System.out.println("Hallo Michael");
+		System.out.println("Hallo Michael");
 	}
 
 }
